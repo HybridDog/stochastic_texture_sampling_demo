6e2c845b5af04453ae6974c211b13ccf0d8e3dc0
HybridDog <ovvv@web.de>
2023-02-22 20:13:27 +0100

    loop function

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,58 @@
 #include "renderer.hpp"
 #include "dtime_calculator.hpp"
 
+struct Context {
+	Renderer &renderer;
+	SDL_Window *window;
+};
+
+bool loop(Context &c)
+{
+	std::array acc_dir{0.0f, 0.0f, 0.0f};
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_KEYDOWN) {
+			switch (event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+				return 0;
+			case SDLK_LEFT:
+				acc_dir[0] -= 1.0f;
+				break;
+			case SDLK_RIGHT:
+				acc_dir[0] += 1.0f;
+				break;
+			case SDLK_UP:
+				acc_dir[1] -= 1.0f;
+				break;
+			case SDLK_DOWN:
+				acc_dir[1] += 1.0f;
+				break;
+			case '+':
+				acc_dir[2] -= 1.0f;
+				break;
+			case '-':
+				acc_dir[2] += 1.0f;
+				break;
+			default:
+				break;
+			}
+		} else if (event.type == SDL_QUIT) {
+			return false;
+		}
+	}
+	//~ float dtime{timer.calculateDtime(SDL_GetTicks64() * 0.001)};
+	//~ renderer.getCamera().update(acc_dir, dtime);
+	c.renderer.getCamera().update(acc_dir, 0.017);
+	c.renderer.render();
+	SDL_GL_SwapWindow(c.window);
+	return true;
+}
+
+void emscripten_loop(void *arg)
+{
+	Context *c{static_cast<Context *>(arg)};
+	loop(*c);
+}
 
 int main()
 {
@@ -20,44 +72,11 @@ int main()
 	if (err != GLEW_OK)
 		return 1;
 	Renderer renderer{800, 600};
-	DtimeCalculator timer{SDL_GetTicks64() * 0.001};
+	Context c{renderer, window};
+	// DtimeCalculator timer{SDL_GetTicks64() * 0.001};
 	for(;;) {
-		std::array acc_dir{0.0f, 0.0f, 0.0f};
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_KEYDOWN) {
-				switch (event.key.keysym.sym) {
-				case SDLK_ESCAPE:
-					return 0;
-				case SDLK_LEFT:
-					acc_dir[0] -= 1.0f;
-					break;
-				case SDLK_RIGHT:
-					acc_dir[0] += 1.0f;
-					break;
-				case SDLK_UP:
-					acc_dir[1] -= 1.0f;
-					break;
-				case SDLK_DOWN:
-					acc_dir[1] += 1.0f;
-					break;
-				case '+':
-					acc_dir[2] -= 1.0f;
-					break;
-				case '-':
-					acc_dir[2] += 1.0f;
-					break;
-				default:
-					break;
-				}
-			} else if (event.type == SDL_QUIT) {
-				return 0;
-			}
-		}
-		float dtime{timer.calculateDtime(SDL_GetTicks64() * 0.001)};
-		renderer.getCamera().update(acc_dir, dtime);
-		renderer.render();
-		SDL_GL_SwapWindow(window);
+		if (!loop(c))
+			break;
 	}
 	return 0;
 }
